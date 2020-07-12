db1f0f41743f626625bbef60a77410fe4f0f8ad2
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-07-12 15:36:15 +0800

    Basically complete

---
+++ src/main/java/com/winnerwinter/springwebcontent/SpringWebContentApplication.java
@@ -0,0 +1,13 @@
+package com.winnerwinter.springwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebContentApplication.class, args);
+	}
+
+}
